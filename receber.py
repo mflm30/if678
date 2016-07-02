926ba291b9201ad054d59475bde9fcd04fe40045
heitorcfelix <heitorcfelix@gmail.com>
2016-07-02 18:12:36 -0300

    Update receber.py

--- receber.py
+++ receber.py
@@ -6,37 +6,44 @@ class Arquivo(object):
 		self.nome = nome
 		self.tamanho = tamanho
 		self.npacotes = int(math.ceil(float(tamanho)/1024)) #tamanho do pacote = 1024bytes = 1KB
-		self.arquivo = []
+		self.arquivo = [0 for i in range(self.npacotes)]
 
 	def printlista(self):
 		print self.nome
 		print self.tamanho
 		print self.npacotes
-		print self.arquivo
+		print len(self.arquivo)
 
-	def inserir(self):
-		pass
+	def inserir(self,indice,data):
+		self.arquivo[indice] = data
 
-
+	def montar(self):
+		nomesaida = ("./" + self.nome)
+		saida = open(nomesaida,'wb')
+		for i in self.arquivo: saida.write(i)
+		saida.close()
 
 
 tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+tcp.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 host = 'localhost'
-port = 6000
+port = 4587
 conexao = (host,port)
 tcp.connect (conexao)
 
-nome = "roberto.jpg"
-nomesaida = ("./" + nome)
-saida = open (nomesaida,'wb')
+#saida = open (nomesaida,'wb')
+nome = "roberto-saida.jpg"
+arquivo = Arquivo(nome,1079915)
+
+indice = int(tcp.recv(1024))
 data = tcp.recv(1024)
 while data != '':
-	saida.write(data)
+	arquivo.inserir(indice,data)
+#	saida.write(data)
 	data = tcp.recv(1024)
+	indice+=1
+arquivo.montar()
 # arquivorecebido = data.split('\n')
 # nome,tamanho=arquivorecebido[5].split(' ')
-
-# testezao.printlista()
-
-saida.close()
+arquivo.printlista()
 tcp.close()
