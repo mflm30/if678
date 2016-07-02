8525dd4931744d3704de5c28b4018b8852d49a97
mflm30 <mflm@cin.ufpe.br>
2016-07-02 13:23:21 -0300

    cliente

--- example/p2p.py
+++ example/p2p.py
@@ -4,112 +4,78 @@ import socket
 import threading
 import select
 import time
+import netifaces # pegar o ip do cliente pae
 
 #classe princiapal pae
 def main():
 
-    class Server(threading.Thread):
-            # propriedados
-            clientes = [];
-
-            # metodos
-            def __init__(self):
-                threading.Thread.__init__(self)
-                self.running = 1
-                self.conn = None
-                self.addr = None
-            def run(self):
-                HOST = ''
-                PORT = 1776
-                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-                s.bind((HOST,PORT))
-                s.listen(13) # é 13 porra
-                self.conn, self.addr = s.accept()
-                # Select loop for listen
-                while self.running == True:
-                    inputready,outputready,exceptready \
-                      = select.select ([self.conn],[self.conn],[])
-                    for input_item in inputready:
-                        # Handle sockets
-                        data = self.conn.recv(1024)
-                        if data:
-                            clientes.append(data) # adicionando um cliente na lista
-                        else:
-                            break
-                    # não sei para que serve esse sleep
-                    time.sleep(0)
-            def kill(self):
-                self.running = 0
-     
     class cliente(threading.Thread):
+            C_IP   = netifaces.ifaddresses('eth0')[2][0]['addr']
+            C_PORT = 2345 #meia 78, ta na hora de molhar o biscoito
+            PORT = 1776 #porta do servidor, a de cima e a do cliente
+
             def __init__(self):
                 threading.Thread.__init__(self)
-                self.host = None
-                self.sock = None
-                self.running = 1
-            def run(self):
-                PORT = 1776
-                self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                self.sock.connect((self.host, PORT))
-                # Select loop for listen
-                while self.running == True:
-                    inputready,outputready,exceptready \
-                      = select.select ([self.sock],[self.sock],[])
-                    # pegando o arquivo pae
-                    arq = open('foo.tar.gz', 'w')
-                    # enviando em pacotes pae
-                    while 1:
-                        dados = conn.recv(1024)
-                        if not dados:
-                            break
-                        arq.write(dados)
-                    # fechando o arquivo pae
-                    arq.close()
-                    time.sleep(0)
-            def send(self):
-                HOST = ''
-                PORT = 1776
+                self.host = '127.0.0.1' # localhost pae, ip do servidor e fixo
+                self.sock = None #sei o que é isso não
+                self.running = 1 #roda o loop da comunicação
+                # enviando o IP para o servidor, e avisando que ta no ar
                 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-                s.bind((HOST,PORT))
-                s.listen(13) # é 13 porra
+                try:
+                    s.bind((HOST,PORT))
+                except Exception, e:
+                    print "Chorou :" +e
+                else:
+                    print "BIURR"
+                finally:
+                    s.listen(1) # é 13 porra, biur
                 self.conn, self.addr = s.accept()
-                # Select loop for listen
-                while self.running == True:
-                    inputready,outputready,exceptready \
-                      = select.select ([self.conn],[self.conn],[])
-                    # pegando o arquivo pae
-                    arq = open('financeiro.tar.gz', 'r')
-                    # enviando em pacotes pae
-                    for i in arq.readlines():
-                        s.send(i)
-                    # fechando o arquivo pae
-                    arq.close()
-                    # não sei para que serve esse sleep
-                    time.sleep(0)
+                conn.send(C_IP+"|"C_PORT) # enviando o IP|PORTA para o servidor
+            def enviar(self):
+                tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                e_host = 'localhost'
+                e_port = 6000
+                try:
+                    conexao = (e_host,e_port)
+                    tcp.bind (conexao)
+                except socket.error as e:
+                    print (e)
+                    exit(0)
+                # ctrl + v
+                tcp.listen(1)
+                conn, addr = tcp.accept()
+                arquivo = open ('roberto-macho.jpg','rb')
+                try:
+                    pacote = arquivo.read(1024)
+                    while pacote != '':
+                        conn.send(pacote)
+                        pacote = arquivo.read(1024)
+                finally:
+                    arquivo.close()
+                    tcp.close()
+                    conn.close()
+            def recebe(self):
+                tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                r_host = 'localhost'
+                r_port = 6000
+                conexao = (r_host,r_port)
+                tcp.connect (conexao)
+                # ctrl + v
+                nome = "roberto.jpg"
+                nomesaida = ("./" + nome)
+                saida = open (nomesaida,'wb')
+                data = tcp.recv(1024)
+                while data != '':
+                    saida.write(data)
+                    data = tcp.recv(1024)
+                # arquivorecebido = data.split('\n')
+                # nome,tamanho=arquivorecebido[5].split(' ')
+                # testezao.printlista()
+                saida.close()
+                tcp.close()
             def kill(self):
                 self.running = 0
-                
-    class arquivo(threading.Thread):
-            def __init__(self):
-                threading.Thread.__init__(self)
-                self.running = 1
-            def run(self):
-                while self.running == True:
-                  text = raw_input('')
-                  try:
-                      chat_client.sock.sendall(text)
-                  except:
-                      Exception
-                  try:
-                      chat_server.conn.sendall(text)
-                  except:
-                      Exception
-                  time.sleep(0)
-            def kill(self):
-                self.running = 0
-    # ex 
 
 if __name__ == "__main__":
     main()
