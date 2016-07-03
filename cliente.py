dd4833a2860d51b59414f2467c0b308773609af2
heitorcfelix <heitorcfelix@gmail.com>
2016-07-03 01:13:27 -0300

    Update cliente.py

--- cliente.py
+++ cliente.py
@@ -11,14 +11,53 @@ class Ping(object):
 		self.quero = quero
 		self.tenho = tenho
 
-identificador = (str(arquivo.readline()).replace("\n",''),str(arquivo.readline()).replace("\n",''))
-#while arquivo.readline() != "*\n":
-tenho = []
+class Arquivo(object):
+	def __init__ (self,nome,tamanho):
+		self.nome = nome
+		self.tamanho = tamanho
+		self.npacotes = int(math.ceil(float(tamanho)/1024)) #tamanho do pacote = 1024bytes = 1KB
+		self.arquivo = [0 for i in range(self.npacotes)]
+		self.completude = verificarcompletude(self)
+
+	def printlista(self):
+		print self.nome
+		print self.tamanho
+		print self.npacotes
+		print len(self.arquivo)
+
+	def inserir(self,indice,data):
+		self.arquivo[indice] = data
+
+	def montar(self):
+		nomesaida = ("./" + self.nome)
+		saida = open(nomesaida,'wb')
+		for i in self.arquivo: saida.write(i)
+		saida.close()
+
+	def verificarcompletude(self):
+		for i in self.arquivo:
+			if i == 0:
+				return False 
+		self.completude = True
+		return True
+
+#PARTE DO CODIGO SO PARA LER O ARQUIVO DE ENTRADA E ADD EM SERVIDOR(HOST,PORT) , QUERO[] , TENHO[(NOME,TAMANHO)]
+servidor = (str(arquivo.readline()).replace("\n",''),str(arquivo.readline()).replace("\n",''))
+tenho = arquivo.readlines()
 quero = []
-for i in arquivo.readlines():
-	if (i != '*\n'):
-		tenho += [i.replace("\n",'')]
-	else:
-		quero = [i.replace("\n",'')]
-print tenho
-print quero
+asterisco = False
+contarDels = 1
+for i in range(len(tenho)):
+	tenho[i] = str(tenho[i]).replace("\n",'')
+	if asterisco == True:
+		quero += [tenho[i]]
+		contarDels += 1
+	if tenho[i]=="*":
+		asterisco = True
+for i in range(contarDels):
+	tenho.pop(len(tenho)-1)
+for i in range(len(quero)):
+	auxiliar = quero[i].split(' ')
+	quero[i] = tuple(auxiliar)
+arquivo.close()
+#-----------------------------------------------
